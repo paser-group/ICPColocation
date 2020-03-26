0f31113ad3d5efe0ea9f577fab20a55414d1a0f8
Akond Rahman <akond.rahman.buet@gmail.com>
2020-03-26 11:24:23 -0500

    save rq2 changes

--- rq2.chars.py
+++ rq2.chars.py
@@ -7,6 +7,9 @@ Characterizing scripts
 import pandas as pd 
 import numpy as np 
 from collections import Counter 
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
 def getFilesWithSameColocations(df_param):
     file_names = np.unique( df_param['FILEPATH'].tolist() )
@@ -26,6 +29,41 @@ def getFilesWithSameColocations(df_param):
                     file_dict[k_]     = file_dict[k_] + [file_name]          
     return file_dict 
 
+
+def getFilesWithDiffColocations(colocation_df):
+    arm_list_of_list = [] 
+    file_names = np.unique( colocation_df['FILEPATH'].tolist() )    
+    for file_name in file_names:
+        per_file_df = colocation_df[colocation_df['FILEPATH']==file_name]
+        icp_list    = per_file_df['TYPE'].tolist()
+        arm_list_of_list.append(icp_list) 
+
+    te = TransactionEncoder()
+    te_ary = te.fit(arm_list_of_list).transform(arm_list_of_list)
+    df = pd.DataFrame(te_ary, columns=te.columns_)
+    frequent_itemsets = apriori(df, min_support=0.0001, use_colnames=True)   ## do not change: min_support=0.0001 
+
+    dict_itemsets = dict( frequent_itemsets.to_dict() ) 
+    support_dict = dict_itemsets['support']
+    itemset_dict = dict_itemsets['itemsets']    
+    identifiers  = support_dict.keys()
+    len_items_dict, colocation_dict, file_dict  = {}, {}, {}
+    for ID in identifiers:
+        support_val = support_dict[ID]
+        itemset_val = itemset_dict[ID]
+        itemset_len = len(itemset_val) 
+        # support count for security smell
+        if itemset_len > 1:
+            if itemset_len not in len_items_dict:
+                len_items_dict[itemset_len] = [support_val] 
+                colocation_dict[itemset_len] = [itemset_val]
+            else: 
+                len_items_dict[itemset_len] = len_items_dict[itemset_len]   +  [support_val]        
+                colocation_dict[itemset_len] = colocation_dict[itemset_len] + [itemset_val] 
+        # support count for files 
+    return  colocation_dict 
+
+
 def getColocationMapping(colocation_file, full_file):
     colocation_df = pd.read_csv(colocation_file)
     full_df       = pd.read_csv(full_file) 
@@ -42,6 +80,8 @@ def getColocationMapping(colocation_file, full_file):
     SAME_COLOCATION_DICT = getFilesWithSameColocations(colocation_df) 
     files_with_same_colocation = SAME_COLOCATION_DICT.values() 
 
+    DIFF_COLOCATION_DICT = getFilesWithDiffColocations(colocation_df) 
+    files_with_diff_colocation = DIFF_COLOCATION_DICT.values() 
 
 
 if __name__=='__main__':
