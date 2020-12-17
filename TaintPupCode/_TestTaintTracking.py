7d758d96fdd43b9999be5ce56418a60c597090be
Akond Rahman <akond.rahman.buet@gmail.com>
2020-12-17 12:15:27 -0600

    hop count: how many vars are tainted before reaching sink: implementation and testing done

--- TaintPupCode/_TestTaintTracking.py
+++ TaintPupCode/_TestTaintTracking.py
@@ -64,7 +64,7 @@ class TestTaintGraph( unittest.TestCase ):
         self.assertEqual( 9 , len(http_taint_dict['$cinder_protocol']) , _test_constants._tainted_http_msg_v3)         
         graph.var_tracker_list.clear()                
 
-    def testTaintedHopCount(self):            
+    def testTaintedHopCountV1(self):            
         _, _, dict_all_attr, dict_all_vari, _, _, _ = parser.executeParser( _test_constants._multi_taint_script_name ) 
         _, http_dict_vars =  orchestra.finalizeHTTP( dict_all_attr, dict_all_vari )  
         http_taint_dict = graph.trackTaint( _test_constants.OUTPUT_HTTP_KW, http_dict_vars, dict_all_attr, dict_all_vari )
@@ -75,6 +75,12 @@ class TestTaintGraph( unittest.TestCase ):
         self.assertEqual( 2 , http_taint_dict['$dashboard_link'][0][-1] , _test_constants.common_error_string + str(2) )         
         graph.var_tracker_list.clear()                
 
+    def testTaintedHopCountV2(self):            
+        _, _, dict_all_attr, dict_all_vari, _, _, _ = parser.executeParser( _test_constants._taintedHttp_script_v3 ) 
+        _, http_dict_vars =  orchestra.finalizeHTTP( dict_all_attr, dict_all_vari )  
+        http_taint_dict = graph.trackTaint( _test_constants.OUTPUT_HTTP_KW, http_dict_vars, dict_all_attr, dict_all_vari )
+        self.assertEqual( 1 , http_taint_dict['$cinder_protocol'][0][-1] , _test_constants.common_error_string + str(1) )         
+        graph.var_tracker_list.clear()                
 
 def checkVarInSmellDict(  dic_smell  ):
         status = False
